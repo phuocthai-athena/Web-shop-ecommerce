9af2186c926b4c8124a031a7e9346ae42bcf03f1
ThaiBP <thaizzdn1999@gmail.com>
2022-08-01 20:52:15 +0700

    #commit make-header

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -13,8 +13,8 @@ const Layout = () => {
       <Route
         render={(props) => (
           <div>
+            <Header {...props} />
             <div className="container">
-              <Header {...props} />
               <div className="main">
                 <Routes />
               </div>
